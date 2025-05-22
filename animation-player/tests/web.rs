5d17d452cdd80b27d84e1e2e114cca8396bf826c
Victor Paléologue <victor.paleologue@semio.ai>
2025-05-22 11:21:33 +0200

    Try fixing browser tests

--- animation-player/tests/web.rs
+++ animation-player/tests/web.rs
@@ -7,7 +7,7 @@ use std::collections::HashMap;
 use uuid::Uuid;
 use wasm_bindgen_test::*;
 
-wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
